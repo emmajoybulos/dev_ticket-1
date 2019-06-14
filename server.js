4a44b76dc9261d5e8a8d89db053d7be6362d1d2f
Emma Bulos <Emma.Bulos@nchcss.com>
2019-06-14 06:53:33 +0100

    mLab connection

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const items = require("./routes/api/items");
+
 const app = express();
+
+//bodyParser
+app.use(bodyParser.json());
+
+// db
+const db = require("./config/keys").mongoURI;
+
+//connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use("api/items", items);
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
