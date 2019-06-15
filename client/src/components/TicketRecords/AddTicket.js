2bf245bd6e3c739af0ea4bb08ab058b294196f4f
unknown <lalata.remo@gmail.com>
2019-06-15 17:00:44 +0800

    updates

--- client/src/components/TicketRecords/AddTicket.js
+++ client/src/components/TicketRecords/AddTicket.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Button, Input, InputGroup,InputGroupAddon } from 'reactstrap';
 
-const addTicket = () =>  (
+const addTicket = (props) =>  (
     <InputGroup>
-        <Input id="appendedInputButton" className="form-control-sm" size="16" type="text" placeholder="Ticket ID"/>
+        <Input 
+            id="appendedInputButton" 
+            className="form-control-sm" 
+            size="16" 
+            type="text" 
+            placeholder="Ticket ID"
+            onChange={props.change}
+            />
         <InputGroupAddon addonType="append">
-        <Button color="primary" size="sm">
+        <Button 
+            color="primary" 
+            size="sm"
+            onClick={props.click}
+            >
             <i className="fa fa-plus"></i>
         </Button>
         </InputGroupAddon>
--- client/src/containers/TicketRecords/TicketRecords.js
+++ client/src/containers/TicketRecords/TicketRecords.js
@@ -5,12 +5,24 @@ import TicketRecordsTable from '../../components/TicketRecords/TicketRecordsTabl
 import AddTicket from '../../components/TicketRecords/AddTicket';
 
 class TicketRecords extends Component {
+    state = {
+        ticket_id: '',
+    }
+
+    handleChange = (event) => {
+        this.setState({ ticket_id: event.target.value });
+    }
+
+    handleClick = (event) => {
+        console.log(this.state.ticket_id);
+    }
+    
     render() {
         return (
             <Card>
                 <CardHeader>
                     <div className="card-header-actions">
-                        <AddTicket />
+                        <AddTicket change={this.handleChange} click={this.handleClick} />
                     </div>
                 </CardHeader>
                 <CardBody>
