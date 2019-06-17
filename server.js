d3adb853a214c347aa9f52c9c8015b70f41f8ce4
unknown <lalata.remo@gmail.com>
2019-06-17 21:31:02 +0800

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require('path');
 
 const items = require("./routes/api/items");
 const tickets = require("./routes/api/tickets");
@@ -27,6 +28,16 @@ mongoose
 app.use("api/items", items);
 app.use("/tickets", tickets);
 
-const port = 5000;
+if(process.env.NODE_ENV == 'production') {
+  
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
