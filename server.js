90007afc530c41d961def4a9d7e4e32df077ce55
unknown <lalata.remo@gmail.com>
2019-06-17 21:57:23 +0800

    commit

--- server.js
+++ server.js
@@ -28,16 +28,12 @@ mongoose
 app.use("api/items", items);
 app.use("/tickets", tickets);
 
-if(process.env.NODE_ENV == 'production') {
-  
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-
-}
-
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
