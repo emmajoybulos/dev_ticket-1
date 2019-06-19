01cbaa58c63f44d1c063726f25304c6231ab1ec9
Remo Lalata <Remo.Lalata@nchcss.com>
2019-06-19 09:26:53 +0100

    for staging

--- server.js
+++ server.js
@@ -30,6 +30,12 @@ app.use("api/items", items);
 app.use("/tickets", tickets);
 app.use("/calendar", calendar);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+ app.use(express.static(path.join(__dirname, 'client', 'build')));	
+
+ app.get('*', (req, res) => {	
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));	
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
